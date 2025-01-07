02bfccf1456c874f7f5440a7f358f4912afe5e0d
Yameeeh <m.path98@gmail.com>
2025-01-07 20:10:17 +0100

    counteract too fast page skipping

--- src/App/GatherData.java
+++ src/App/GatherData.java
@@ -40,6 +40,8 @@ public class GatherData {
 				try {
 					WebElement nextPageButton = driver.findElement(By.cssSelector("[aria-label='Next page']"));
 					if (nextPageButton.isEnabled()) {
+						// short sleep to ensure the page isn't skipped too early
+						Thread.sleep(50);
 						nextPageButton.click();
 						wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("mud-table-row")));
 					} else {
