96f05d7edf150c6e97af46d61846949e4fa420af
Yameeeh <m.path98@gmail.com>
2025-01-07 20:31:09 +0100

    optimization and console cleanup

--- src/App/MainClass.java
+++ src/App/MainClass.java
@@ -17,20 +17,22 @@ public class MainClass {
 
 		List<Monster> monsters = GatherData.parse(url, level);
 
-		int i = 1;
+		DecimalFormatSymbols symbols = new DecimalFormatSymbols(Locale.US);
+		DecimalFormat efficiencyFormat = new DecimalFormat("#.##", symbols);
+		DecimalFormat damageFormat = new DecimalFormat("#,###", symbols);
 
+		int i = 1;
 		for (Monster monster : monsters) {
 			if (i == 1) {
 				monster.setEfficiency(1.00);
 			} else {
 				monster.setEfficiency(monsters.get(0).getRelativeDamage() / monster.getRelativeDamage());
 			}
-			DecimalFormatSymbols symbols = new DecimalFormatSymbols(Locale.US);
-			DecimalFormat efficiencyFormat = new DecimalFormat("#.##", symbols);
-			DecimalFormat damageFormat = new DecimalFormat("#,###", symbols);
-			System.out.println("#" + i + " " + monster.getName() + ", " + monster.getElement() + ", lvl "
-					+ monster.getLvl() + ", Relative Damage per %: " + damageFormat.format(monster.getRelativeDamage())
-					+ ", Efficiency: " + efficiencyFormat.format(monster.getEfficiency() * 100) + "%");
+
+			System.out.println("#" + String.format("%02d", i) + " " + monster.getName() + ", " + monster.getElement()
+					+ ", lvl " + monster.getLvl() + ", Relative Damage per %: "
+					+ damageFormat.format(monster.getRelativeDamage()) + ", Efficiency: "
+					+ efficiencyFormat.format(monster.getEfficiency() * 100) + "%");
 			i++;
 		}
 
