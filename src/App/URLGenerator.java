96b8ca99d4fd958dd45438a2e4026131aedaea18
Yameeeh <m.path98@gmail.com>
2025-01-09 01:14:45 +0100

    remove debug log

--- src/App/URLGenerator.java
+++ src/App/URLGenerator.java
@@ -12,7 +12,6 @@ public class URLGenerator {
 		for (String area : areas) {
 			areaJoiner.add(area);
 		}
-		System.out.println(areaJoiner.toString());
 
 		return "https://flyffipedia.com/monsters?min=" + minlvlMonster + "&drops=" + hideNoDrops + "&max="
 				+ maxlvlMonster + "&sort=dmg-1&flying=" + includeFlying + "&rank=Normal%2cCaptain%2cSmall"
